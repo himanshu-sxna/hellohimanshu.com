9a78846a8a41df81ca6c80d846bc42e53a008b6b
Himanshu Saxena <contact@hellohimanshu.com>
2021-03-08 23:33:40 +1030

    build page

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,16 @@
 import Head from 'next/head'
+import Wrapper from "../components/Wrapper"
+import IntroSection from "../components/Intro"
 
 export default function Home() {
   return (
     <>
-  
+    <Wrapper>
+      <Head>
+        <title>Himanshu|Web Developer</title>
+      </Head>
+      <IntroSection/>
+    </Wrapper>
     </>
   )
 }

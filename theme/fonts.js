33b363b8e22c9cc15daa32336d1508b308aadf09
Himanshu Saxena <contact@hellohimanshu.com>
2021-03-08 23:31:50 +1030

    removed file

--- theme/fonts.js
+++
@@ -1,7 +0,0 @@
- const fonts = {
-    body: "'Lato', -apple-system, BlinkMacSystemFont, 'Segoe UI', Helvetica, Arial, sans-serif, 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol'",
-    heading: "'Lato', -apple-system, BlinkMacSystemFont, 'Segoe UI', Helvetica, Arial, sans-serif, 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol'",
-    mono: "'Space Mono', -apple-system, BlinkMacSystemFont, 'Segoe UI', Helvetica, Arial, sans-serif, 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol'",
-}
-
-export default fonts

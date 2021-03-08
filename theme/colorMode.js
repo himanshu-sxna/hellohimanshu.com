b49a9332908c35ad609ce7b781c33cf7b7da843a
Himanshu Saxena <contact@hellohimanshu.com>
2021-03-08 23:33:57 +1030

    color modes for light/dark theme

---
+++ theme/colorMode.js
@@ -0,0 +1,24 @@
+const colorModes = {
+    heading: {
+        light: "black",
+        dark: "white"
+    },
+    text: {  
+        light:"gray.600",
+        dark:"gray.400"
+    },
+    iconColor:  {
+        light: 'gray.900',
+        dark: 'gray.50'
+    },
+    iconBgColor: {   
+        light:'gray.50',
+        dark:'gray.900'
+    },
+    borderColor: {
+        light: 'gray.200',
+        dark: 'gray.800'
+    }
+}
+
+export default colorModes

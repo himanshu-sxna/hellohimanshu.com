48962ad696ef2a7bd5dbfdf54761aa3c06600697
Himanshu Saxena <contact@hellohimanshu.com>
2021-03-09 23:45:36 +1030

    content for app display section

---
+++ content/work.js
@@ -0,0 +1,36 @@
+const workItems = [
+    {
+        title: 'SMB Template',
+        description: 'Designed to fast track the online presence of any business in a hassle free way to get them up and running with a landing page, blog and an online store.',
+        tech: '#React #MongoDB #Node.js #Express.js #Snipcart',
+        image: '/images/smbTemp.jpg',
+        link: 'https://smb-template.herokuapp.com/',
+        bgColor: '#cacef4',
+    },
+    { 
+        title: 'hellohimanshu.com',
+        description: 'The clean looking webiste you are browsing now.',
+        tech: '#Next.js #ChakraUI',
+        image: '/images/hellohimanshu.jpg',
+        link: 'https://hellohimanshu.com/',
+        bgColor: '#f3e7e0'
+    },
+    {
+        title: 'PERsoNAL WORKOUT TRACKER',
+        description: 'Store and track your workouts and display workout with charts.',
+        tech: '#JavaScript #MongoDB #Node.js #Heroku',
+        image: '/images/workout.jpg',
+        link: 'https://thawing-hollows-58627.herokuapp.com/?id=5ffc79217c931d00172f0dfa',
+        bgColor: '#f0ffea'
+    },
+    {
+        title: 'GET YOUR WEATHER',
+        description: "A weather dashboard that allows the user to search for any city's current weather condition, UV Index and the next 5 days' forecast.",
+        tech: '#JavaScript #HTML #CSS #OpenWeather',
+        image: '/images/weather.jpg',
+        link: 'https://himanshu-sxna.github.io/weather-dashboard/',
+        bgColor: '#eaf8ff'
+    }
+]
+
+export default workItems

fcf54718b9ba2ec40b72b73b4afaddc194f8d817
Himanshu Saxena <contact@hellohimanshu.com>
2021-03-09 23:43:37 +1030

    update fonts

--- theme/index.js
+++ theme/index.js
@@ -3,10 +3,10 @@ import { extendTheme } from '@chakra-ui/react'
 const custTheme =  extendTheme({
     textStyles: {
         h1: {
-          fontFamily:"'Lato', -apple-system, BlinkMacSystemFont, 'Segoe UI', Helvetica, Arial, sans-serif, 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol'",
+          fontFamily:"'Kumbh Sans', -apple-system, BlinkMacSystemFont, 'Segoe UI', Helvetica, Arial, sans-serif, 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol'",
         },
         p:  {
-          fontFamily:"'Lato', -apple-system, BlinkMacSystemFont, 'Segoe UI', Helvetica, Arial, sans-serif, 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol'",
+          fontFamily:"'Kumbh Sans', -apple-system, BlinkMacSystemFont, 'Segoe UI', Helvetica, Arial, sans-serif, 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol'",
         },
         mono: {
             fontFamily:"'Space Mono', -apple-system, BlinkMacSystemFont, 'Segoe UI', Helvetica, Arial, sans-serif, 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol'",

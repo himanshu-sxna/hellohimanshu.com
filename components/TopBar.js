ce779d8a5949e0256b49b39ef53786370c6f0258
Himanshu Saxena <contact@hellohimanshu.com>
2021-03-09 23:45:25 +1030

    update theme toggle icon

--- components/TopBar.js
+++ components/TopBar.js
@@ -1,13 +1,21 @@
-import { Flex, Spacer, Box } from "@chakra-ui/react"
+import { Flex, Spacer, Box, useColorMode } from "@chakra-ui/react"
+import colorModes from '../theme/colorMode'
 import ThemeToggle from "./ThemeToggle"
 
 const TopBar = () => {
+    const { colorMode } = useColorMode()
+
     return (
         <Flex
          mt={10}
         >
-            <Box>
-             {/*Dummy div */}
+            <Box
+                as="p"
+                textStyle="p"
+                fontSize="5xl"
+                color={colorModes.heading[colorMode]}
+            >
+             h.
             </Box>
             <Spacer />
             <Box>

42fef2291b5e0b3b9434b38bbc6ed2a8991e2091
Himanshu Saxena <contact@hellohimanshu.com>
2021-03-09 23:45:49 +1030

    updated title tag

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,19 @@
 import Head from 'next/head'
 import Wrapper from "../components/Wrapper"
 import IntroSection from "../components/Intro"
+import ProjectSection from "../components/Projects"
+import Footer from "../components/Footer"
 
 export default function Home() {
   return (
     <>
     <Wrapper>
       <Head>
-        <title>Himanshu|Web Developer</title>
+        <title>Himanshu | Full Stack Web Developer</title>
       </Head>
       <IntroSection/>
+      <ProjectSection/>
+      <Footer/>
     </Wrapper>
     </>
   )

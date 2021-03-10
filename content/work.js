e571cd308040e3132df9762f46d5ff9dcad6c330
Himanshu Saxena <contact@hellohimanshu.com>
2021-03-10 13:30:36 +1030

    fixed typos

--- content/work.js
+++ content/work.js
@@ -1,7 +1,7 @@
 const workItems = [
     {
         title: 'SMB Template',
-        description: 'Designed to fast track the online presence of any business in a hassle free way to get them up and running with a landing page, blog and an online store.',
+        description: 'Designed to fast track the online presence of businesses in a hassle free way to get them up and running with a landing page, blog and an online store.',
         tech: '#React #MongoDB #Node.js #Express.js #Snipcart',
         image: '/images/smbTemp.jpg',
         link: 'https://smb-template.herokuapp.com/',
@@ -9,7 +9,7 @@ const workItems = [
     },
     { 
         title: 'hellohimanshu.com',
-        description: 'The clean looking webiste you are browsing now.',
+        description: 'The clean looking website you are browsing now.',
         tech: '#Next.js #ChakraUI',
         image: '/images/hellohimanshu.jpg',
         link: 'https://hellohimanshu.com/',
@@ -17,7 +17,7 @@ const workItems = [
     },
     {
         title: 'PERsoNAL WORKOUT TRACKER',
-        description: 'Store and track your workouts and display workout with charts.',
+        description: 'Track and store your workouts, display exercise data with charts.',
         tech: '#JavaScript #MongoDB #Node.js #Heroku',
         image: '/images/workout.jpg',
         link: 'https://thawing-hollows-58627.herokuapp.com/?id=5ffc79217c931d00172f0dfa',
@@ -25,7 +25,7 @@ const workItems = [
     },
     {
         title: 'GET YOUR WEATHER',
-        description: "A weather dashboard that allows the user to search for any city's current weather condition, UV Index and the next 5 days' forecast.",
+        description: "A weather dashboard to search and display any city's current weather, UV Index and the next 5 days' forecast.",
         tech: '#JavaScript #HTML #CSS #OpenWeather',
         image: '/images/weather.jpg',
         link: 'https://himanshu-sxna.github.io/weather-dashboard/',

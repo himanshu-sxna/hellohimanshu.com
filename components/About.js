9cdad44e9bd86da07ba981450772d6b95ee4da37
Himanshu Saxena <contact@hellohimanshu.com>
2021-03-11 08:22:10 +1030

    chnage icon onClick via useState

--- components/About.js
+++ components/About.js
@@ -1,16 +1,32 @@
-import { HStack, Tooltip, useColorMode, Popover,
-    Accordion,
-  AccordionItem,
-  AccordionButton,
-  AccordionPanel,
-  AccordionIcon,
-    Button } from "@chakra-ui/react"
+import { useState } from "react"
+import { Text, useColorMode, Accordion, AccordionItem, AccordionButton, AccordionPanel } from "@chakra-ui/react"
 import colorModes from '../theme/colorMode'
 
 const AboutSection = () => {
     const { colorMode } = useColorMode()
 
+    const [ aboutIcon, setAboutIcon ] = useState('👇')
+
+    function switchAboutIcon() { 
+        if (aboutIcon === "👇") {
+            setAboutIcon("🙂")
+        } else if (aboutIcon === "🙂"){
+            setAboutIcon("👇")
+        } else {
+            setAboutIcon("👇")
+        }
+    }
+
     return(
+        <>
+        <Text
+            color={colorModes.text[colorMode]}
+            textStyle="mono"
+            fontSize="md"
+            mb={2}
+        >
+            Click {aboutIcon} to know more about me!
+        </Text>
         <Accordion allowToggle>
                 <AccordionItem>
                     <AccordionButton
@@ -19,6 +35,7 @@ const AboutSection = () => {
                         _focus={{borderColor:'none'}}
                         p={0}
                         mb={3}
+                        onClick={switchAboutIcon}
                     >
                         👨‍👩‍👧  💻  🏠 
                     </AccordionButton>
@@ -43,13 +60,14 @@ const AboutSection = () => {
                         <br/>
                         🍕 Food: Pizza
                         <br/>
-                        🤸‍♂️ Play: Badminton, Cycling & Swimming
+                        🤸‍♂️ Play: 🏸 🚴‍♂️ 🏊‍♂️
                         <br/>
                         🛠️ DIY Enthusiast
                         <br/>
-                    </AccordionPanel>
-                </AccordionItem>
-            </Accordion>
+                </AccordionPanel>
+            </AccordionItem>
+        </Accordion>
+        </>
     )
 }
 
